141078d9128612dcc6640bad9893b82a162c8a51
ZahidCodes <zahidheir1234@gmail.com>
2023-02-26 22:02:41 +0530

    task4

--- main.js
+++ main.js
@@ -21,23 +21,42 @@
 
 
  //console. log(document. getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
 //  console.log(headerTitle);
 //  headerTitle.textContent = 'Hello';
 //  headerTitle.innerText = 'Goodbye';
 //  console.log(headerTitle. innerText);
 //  headerTitle.innerHTML = '<h3>Hello</h3>`;
-header.style.borderBottom = 'solid 3px #000';
+// header.style.borderBottom = 'solid 3px #000';
 
 // var textTitle= document.getElementsByClassName('title');
 // textTitle.style.fontWeight='bold';
 
-var titles = document.querySelectorAll( '.title');
-console.log(titles);
-titles[0].textContent = 'Hello';
-titles[0].style.fontWeight='bold';
-titles[0].style.color='green';
+// var titles = document.querySelectorAll( '.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+// titles[0].style.fontWeight='bold';
+// titles[0].style.color='green';
+
+// GETELEMENTSBYCLASSNAME
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent= 'Hello 2';
+items[0].style.fontWeight = 'bold';
+items[1].style.fontWeight = 'bold';
+items[2].style.fontWeight = 'bold';
+items[2].style.fontWeight = 'bold';
+
+items[2].style.backgroundColor= 'green';
+
+// Gives error
+//items.style.backgroundColor = *#f4f4f4';
+// for(var i = 0; i < items.length; i++){
+//  items[i].style.backgroundColor= '#f4f4f4';
+// }
+
 
 
 
